175ee57721c6d354558f33aaf0ef5e43fa503622
Naughtyboykarthik <135523719+Naughtyboykarthik@users.noreply.github.com>
2023-06-12 20:32:04 +0530

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>WELCOME</div>
+    <header>
+      <h1>WELCOME</h1>
+        <ul className="header_header">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+    </header>
   )
 }
 
