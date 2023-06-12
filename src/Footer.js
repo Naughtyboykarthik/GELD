1b179fdd6be01378e6bac9dc7813610c63e12f40
Naughtyboykarthik <135523719+Naughtyboykarthik@users.noreply.github.com>
2023-06-12 20:30:45 +0530

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -3,8 +3,14 @@ import React from 'react'
 const Footer = () => {
     const year= new Date();
 
+
+// const hloo = [-2,3,-1,0,1,2]
+// const vanga = hloo.filter((soo) => soo<=0).map((soo) =>({age:soo}))
+// console.log(vanga)
+
+
   return (
-    <div>Footer &copy; {year.getFullYear() }</div>
+    <footer>Footer &copy; {year.getFullYear()}</footer>
   )
 }
 
